7bd60256771e568c4e61e84c14b952a1412f35d9
fulong.shi <fulong.shi@ysura.com>
2021-07-28 08:43:12 +0200

    learn about the infrastructure of the project

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,8 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8080"'
+  // For mall project
+  // BASE_API: '"http://localhost:8080"'
+  // For mall-swarm project
+  BASE_API: '"http://localhost:8201/mall-admin"'
 })
