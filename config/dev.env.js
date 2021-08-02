37d76833735f9e283df99a438aa828b41617289d
fulong.shi <fulong.shi@ysura.com>
2021-08-02 11:26:59 +0200

    update items in package.json with latest compatible version

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,11 +1,11 @@
 'use strict'
-const merge = require('webpack-merge')
+const {merge} = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  // For mall project
-  // BASE_API: '"http://localhost:8080"'
-  // For mall-swarm project
-  BASE_API: '"http://localhost:8201/mall-admin"'
+    NODE_ENV: '"development"',
+    // For mall project
+    // BASE_API: '"http://localhost:8080"'
+    // For mall-swarm project
+    BASE_API: '"http://localhost:8201/mall-admin"'
 })
