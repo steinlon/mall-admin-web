cf0a3af1ba4359655d25e98009adcedb6371c975
fulong.shi <fulong.shi@ysura.com>
2021-10-16 08:17:57 +0200

    Fix and adapt admin view for menu

---
+++ src/views/manage/api/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-loading="loading" :style="'height:'+ height">
+    <iframe :src="src" frameborder="no" style="width: 100%;height: 100%" scrolling="auto"/>
+  </div>
+</template>
+<script>
+export default {
+  name: "API",
+  data() {
+    return {
+      src: process.env.BASE_API.replaceAll("mall-admin", "doc.html"),
+      height: document.documentElement.clientHeight - 94.5 + "px;",
+      loading: true
+    };
+  },
+  mounted: function() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 250);
+    const that = this;
+    window.onresize = function temp() {
+      that.height = document.documentElement.clientHeight - 94.5 + "px;";
+    };
+  }
+};
+</script>
---
+++ src/views/manage/info/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app-container">
+    <span>{{ docs }}</span>
+  </div>
+</template>
+<script>
+import {apiDocs} from "@/api/swagger";
+
+export default {
+  name: "Info",
+  data() {
+    return {
+      docs: null
+    }
+  },
+  created() {
+    this.load();
+  },
+  methods: {
+    load() {
+      apiDocs().then(data => {
+        this.docs = data;
+      });
+    }
+  }
+}
+</script>
+<style></style>
